4ded1e7e8e111a9bae9d53fa266dacd94ee8fcdc
Taras Lyashenko <t.d.liashenko@gmail.com>
2023-08-26 17:34:59 +0600

    Automatic generation of flight code at creation

--- src/main/java/com/example/airlineTicketing/service/impl/FlightServiceImpl.java
+++ src/main/java/com/example/airlineTicketing/service/impl/FlightServiceImpl.java
@@ -4,6 +4,7 @@ import com.example.airlineTicketing.dao.FlightDao;
 import com.example.airlineTicketing.entity.Flight;
 import com.example.airlineTicketing.service.FlightService;
 import jakarta.annotation.Resource;
+import org.apache.commons.lang3.RandomStringUtils;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -15,6 +16,13 @@ public class FlightServiceImpl implements FlightService
     @Override
     public void saveFlight(Flight flight)
     {
+        String codeFlight = generateRandomCode(6);
+        flight.setCode(codeFlight);
         flightDao.save(flight);
+    }
+
+    private String generateRandomCode(int length)
+    {
+        return RandomStringUtils.randomAlphanumeric(length);
     }
 }
